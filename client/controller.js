73cf3f6a1a15546368507900bce9f264bd97e32c
odenthalkay <Kay.Odenthal@gmx.de>
2014-12-29 22:13:44 +0000

    App fungiert jetzt auch als webserver => kann statische Inhalte ausliefern UND REST Endpunkte können jetzt über http angesprochen werden.

--- client/controller.js
+++ client/controller.js
@@ -2,11 +2,11 @@
 
 /* Controllers */
 
-angular.module('myApp.controller', []).
-  controller('myCtrl', function($scope) {
-    
-    $scope.books = ["a","b","c"];
-    $scope.x = "das ist x";
-    
-    
-  });
+angular.module('myApp.controller', []).controller('myCtrl', function($scope, $http) {
+
+    $http.get('/api/books').success(function(data, status, headers, config) {
+        $scope.books = data;
+    }).error(function(data, status, headers, config) {
+        alert(status);
+    });
+});
--- server.js
+++ server.js
@@ -2,33 +2,38 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express'); // call express
+var app = express(); // define our app using express
 var bodyParser = require('body-parser');
 var database = require('./scraper-database.js');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+ console.log(__dirname);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
+app.configure(function () {
+    app.use(
+        "/", //the URL throught which you want to access to you static content
+        express.static(__dirname+"/client") //where your static content is located in your filesystem
+    );
+});
 
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-app.get('/', function(req, res) {
-    
-    
-    database.load('spiegel', function(error, result){
+app.get('/api/books', function(req, res) {
+    database.load('spiegel', function(error, result) {
         if (error) {
             res.json(error);
         }
-        
+
         res.json(result);
     });
-    
-    
-    
-       
 });
+
+
 
 // more routes for our API will happen here
 
