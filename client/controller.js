648a909c519e694b7fb03906e34eb2c5c2e30663
odenthalkay <Kay.Odenthal@gmx.de>
2014-12-29 22:45:43 +0000

    Index.html angepasst, Ergebnisse werden jetzt aufgelistet

--- client/controller.js
+++ client/controller.js
@@ -5,7 +5,8 @@
 angular.module('myApp.controller', []).controller('myCtrl', function($scope, $http) {
 
     $http.get('/api/books').success(function(data, status, headers, config) {
-        $scope.books = data;
+        // Warum ist das Datenobjekt ein Array??
+        $scope.books = data[0];
     }).error(function(data, status, headers, config) {
         alert(status);
     });
